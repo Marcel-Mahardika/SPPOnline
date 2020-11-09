1995d13d12416af99fd3aee14ad87a89042ea5f3
Marcel Mahardika <71962721+Marcel-Mahardika@users.noreply.github.com>
2020-11-09 10:38:30 +0700

    Tugas 4

--- Nama_Aplikasi/src/main/java/module-info.java
+++ Nama_Aplikasi/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.mavenproject4 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
     opens com.mycompany.mavenproject4 to javafx.fxml;
     exports com.mycompany.mavenproject4;
