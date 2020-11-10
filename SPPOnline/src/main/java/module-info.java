f280fc51c62fecdcd2c7d7a4193fd2e9f51dd8e6
Marcel Mahardika <71962721+Marcel-Mahardika@users.noreply.github.com>
2020-11-10 18:12:27 +0700

    Error

--- SPPOnline/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.mavenproject4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-    requires java.logging;
-
-    opens com.mycompany.mavenproject4 to javafx.fxml;
-    exports com.mycompany.mavenproject4;
-}
