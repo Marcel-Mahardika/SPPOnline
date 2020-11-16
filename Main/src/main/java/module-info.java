9c05bbf19a840d086c5e7f74210d50a1f58a7d15
Marcel Mahardika <71962721+Marcel-Mahardika@users.noreply.github.com>
2020-11-16 15:31:00 +0700

    login_logout

--- Main/src/main/java/module-info.java
+++ Main/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.mycompany.main {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.base;
 
     opens com.mycompany.main to javafx.fxml;
     exports com.mycompany.main;
